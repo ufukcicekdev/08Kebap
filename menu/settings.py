35e02180408ef4daf337631f9addd8c285b5641f
EAGLEEARTH <ufukcicek199@gmail.com>
2024-08-01 02:24:49 +0300

    push

--- menu/settings.py
+++ menu/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -96,20 +97,6 @@ WSGI_APPLICATION = "menu.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('PROD_DATABASE_ENGINE'),
-#         'NAME': os.getenv('PROD_DATABASE_NAME'),
-#         'USER': os.getenv('PROD_DATABASE_USER'),
-#         'PASSWORD': os.getenv('PROD_DATABASE_PASSWORD'),
-#         'HOST': os.getenv('PROD_DATABASE_HOST'),
-#         'PORT': os.getenv('PROD_DATABASE_PORT'),
-#         'OPTIONS': {
-#             'sslmode': 'require',  # SSL gereklilik durumu
-#         },
-#     }
-# }
 
 
 
@@ -219,3 +206,14 @@ JAZZMIN_UI_TWEAKS = {
         "success": "btn-success"
     }
 }
+
+
+AWS_ACCESS_KEY_ID = os.getenv('DEV_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('DEV_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('DEV_AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = os.getenv('DEV_AWS_S3_CUSTOM_DOMAIN')
+AWS_DEFAULT_ACL = os.getenv('DEV_AWS_DEFAULT_ACL')
+AWS_S3_REGION_NAME = os.getenv("DEV_AWS_S3_REGION_NAME")
+AWS_S3_ENDPOINT_URL = os.getenv("DEV_AWS_S3_ENDPOINT_URL")
+AWS_LOCATION = os.getenv("DEV_AWS_LOCATION")
+DEFAULT_FILE_STORAGE = 'menu.storage_backends.CustomS3Boto3Storage'
